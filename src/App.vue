d1c89dea10fb57b37762c66e7bee41a06f736e10
santhana krishnan <96221879+krishnan314@users.noreply.github.com>
2023-01-02 15:30:01 +0530

    init and response card design

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+    import Response from './components/Response.vue'
+</script>
+
+<template>
+    <header>
+        <p class="h1 text-center">Plus One</p>
+    </header>
+
+    <main>
+        <div class="container">
+            <Response responseText="Hi" />
+        </div>
+    </main>
+</template>
+
+<style scoped>
+    header {
+        line-height: 1.5;
+    }
+
+    .logo {
+        display: block;
+        margin: 0 auto 2rem;
+    }
+
+    @media (min-width: 1024px) {
+        header {
+            display: flex;
+            place-items: center;
+            padding-right: calc(var(--section-gap) / 2);
+        }
+
+        .logo {
+            margin: 0 2rem 0 0;
+        }
+
+        header .wrapper {
+            display: flex;
+            place-items: flex-start;
+            flex-wrap: wrap;
+        }
+    }
+</style>
---
+++ src/components/Response.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+    responseText: {
+        type: String,
+        required: true,
+    },
+    count: {
+        default: 0,
+        type: Number,
+    },
+});
+</script>
+
+<template>
+<div class="card" style="width: 18rem">
+    <div class="card-body">
+        <p class="card-title"> {{ responseText }} </p>
+        <div class="row justify-content-around">
+            <span class="col">{{ count }} <br/>people</span>
+            <a href="#" 
+               class="round-btn btn btn-primary col"
+            >+1</a>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.round-btn {
+    border-radius: 40px;
+    min-height: 40px;
+    min-width: 40px;
+    max-height: 50px;
+    max-width: 50px;
+    padding-top: 10px;
+}
+
+.card {
+    padding: 10px;
+    border-radius: 40px;
+}
+</style>
