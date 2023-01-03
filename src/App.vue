e7eef2515d6699dfc5549a7d584f643628263163
santhana krishnan <29514440+hashkrish@users.noreply.github.com>
2023-01-03 11:24:13 +0530

    update response to increment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <script setup>
-    import Response from './components/Response.vue'
+
+import { reactive } from "vue";
+import Response from "./components/Response.vue";
+
+let responses = reactive([
+    {
+        id: 0,
+        responseText: "I do not understand this updating part",
+        count: 1,
+    },
+    {
+        id: 1,
+        responseText: "How does x becomes 3?",
+        count: 2,
+    },
+]);
+
+const updateCount = (id, updateType) => {
+    if (updateType === "inc") {
+        responses[id].count++;
+    }
+    else if (updateType === "dec") {
+        responses[id].count--;
+    }
+};
+
 </script>
 
 <template>
-    <header>
-        <p class="h1 text-center">Plus One</p>
-    </header>
+<header>
+    <p class="h1 text-center">Plus One</p>
+</header>
 
-    <main>
-        <div class="container">
-            <Response responseText="Hi" />
+<main>
+    <div class="container">
+        <div class="d-flex justify-content-center flex-wrap">
+            <Response v-for="response in responses"
+                :responseText="response.responseText"
+                :count="response.count"
+                :id="response.id"
+                :key="response.id"
+                @update-count="updateCount"
+            />
         </div>
-    </main>
+    </div>
+</main>
 </template>
 
 <style scoped>
--- src/components/Response.vue
+++ src/components/Response.vue
@@ -1,14 +1,42 @@
 <script setup>
-defineProps({
+import { ref, toRef, toRefs } from "vue";
+
+const props = defineProps({
     responseText: {
         type: String,
         required: true,
     },
     count: {
-        default: 0,
         type: Number,
+        default: 0,
     },
+    include: {
+        type: Boolean,
+        default: null,
+    },
+    id: {
+        type: String,
+        default: "NoID",
+    }
 });
+
+const emits = defineEmits([
+    "incrementCount",
+    "decrementCount"
+]);
+
+const selectedButtonClass = ref("round-btn btn btn-outline-success");
+const countProp = toRef("");
+function inverseSelection() {
+    if (selectedButtonClass.value === "round-btn btn btn-outline-success") {
+        selectedButtonClass.value = "round-btn btn btn-success";
+        emits("updateCount", props.id, "inc");
+    } else {
+        selectedButtonClass.value = "round-btn btn btn-outline-success";
+        emits("updateCount", props.id, "dec");
+    }
+};
+
 </script>
 
 <template>
@@ -16,10 +44,17 @@ defineProps({
     <div class="card-body">
         <p class="card-title"> {{ responseText }} </p>
         <div class="row justify-content-around">
-            <span class="col">{{ count }} <br/>people</span>
-            <a href="#" 
-               class="round-btn btn btn-primary col"
-            >+1</a>
+            <span class="col"> {{ count }} <br/>people</span>
+            <a role="button"
+                data-bs-toggle="button"
+                @click="inverseSelection"
+                :class="selectedButtonClass"
+            >
+                +1
+            </a>
+        </div>
+        <div class="row justify-content-around debug">
+            <span class="col"> Id: {{ id }} </span>
         </div>
     </div>
 </div>
@@ -38,5 +73,12 @@ defineProps({
 .card {
     padding: 10px;
     border-radius: 40px;
+    margin: 10px;
+}
+
+.debug {
+    background-color: #ddd;
+    margin-top: 10px;
+    border-radius: 10px;
 }
 </style>
